45d6a4cb6c083d794306be363fbbb8f8f4d239ef
Enes Yıldırım <enes5519@gmail.com>
2020-01-24 21:11:36 +0300

    Recode for bossbar and compatible with pmmp

--- src/Enes5519/CompassBar/CompassBar.php
+++ src/Enes5519/CompassBar/CompassBar.php
@@ -6,68 +6,99 @@ namespace Enes5519\CompassBar;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\entity\utils\Bossbar;
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntityIds;
+use pocketmine\event\EventPriority;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\network\mcpe\protocol\BossEventPacket;
+use pocketmine\network\mcpe\protocol\RemoveActorPacket;
 use pocketmine\Player;
+use pocketmine\plugin\MethodEventExecutor;
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\TaskHandler;
 use pocketmine\utils\TextFormat;
 
 class CompassBar extends PluginBase implements Listener{
+	/** @var Player[] */
+	public $enabled = [];
+	/** @var int */
+	private $entityId;
 
-    public const BOSSBAR_ID = 500;
+	public function onEnable(){
+		$this->entityId = Entity::$entityCount++;
 
-    /** @var int */
-    public $refreshRate = 4;
-    /** @var TaskHandler[] */
-    protected $barTasks = [];
-    /** @var Bossbar */
-    protected $bossBar;
+		$this->saveDefaultConfig();
 
-    public function onEnable(){
-        @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
+		$refreshRate = (int) $this->getConfig()->get("refresh-rate", 4);
+		if($refreshRate < 1){
+			$this->getLogger()->warning("Refresh rate property in config.yml is less than 1. Resetting to 1");
+			$this->getConfig()->set("refresh-rate", 1);
+			$this->getConfig()->save();
+			$refreshRate = 1;
+		}
 
-        $this->refreshRate = (int) $this->getConfig()->get("refresh-rate", 4);
-        if($this->refreshRate < 1){
-            $this->getLogger()->warning("Refresh rate property in config.yml is less than 1. Resetting to 1");
-            $this->getConfig()->set("refresh-rate", 1);
-            $this->getConfig()->save();
-            $this->refreshRate = 1;
-        }
+		Utils::init();
+		$this->getServer()->getPluginManager()->registerEvent(PlayerQuitEvent::class, $this, EventPriority::NORMAL, new MethodEventExecutor("onQuit"), $this, false);
+		$this->getScheduler()->scheduleRepeatingTask(new ShowBarTask($this), $refreshRate);
+	}
 
-        Utils::init();
-        $this->bossBar = new Bossbar("Loading Compass Bar");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getScheduler()->scheduleRepeatingTask(new ShowBarTask($this), $this->refreshRate);
-    }
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if(!($sender instanceof Player)){
+			$sender->sendMessage(TextFormat::RED . "You can use this command in the game.");
+			return true;
+		}
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if(!($sender instanceof Player)){
-            $sender->sendMessage(TextFormat::RED . "You can use this command in the game.");
-            return true;
-        }
+		if(isset($this->enabled[$sender->getLowerCaseName()])){
+			$this->removeBossBar($sender);
+			$sender->sendMessage(TextFormat::RED . "CompassBar is now off.");
+		}else{
+			$this->enabled[$sender->getLowerCaseName()] = $sender;
 
-        if($sender->getBossbar(self::BOSSBAR_ID) == null){
-            $sender->addBossbar($this->bossBar, self::BOSSBAR_ID);
-            $sender->sendMessage(TextFormat::GREEN . "CompassBar is now on!");
-        }else{
-            $sender->removeBossbar(self::BOSSBAR_ID);
-            $sender->sendMessage(TextFormat::RED . "CompassBar is now off.");
-        }
+			$pk = new AddActorPacket();
+			$pk->entityRuntimeId = $this->entityId;
+			$pk->type = EntityIds::SLIME;
+			$pk->metadata = [
+				Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, ((1 << Entity::DATA_FLAG_INVISIBLE) | (1 << Entity::DATA_FLAG_IMMOBILE))],
+				Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, '']
+			];
+			$pk->position = new Vector3();
+			$sender->sendDataPacket($pk);
 
-        return true;
-    }
+			$this->sendBossPacket($sender, '', BossEventPacket::TYPE_SHOW);
 
-    /**
-     * @return Bossbar
-     */
-    public function getBossBar() : Bossbar{
-        return $this->bossBar;
-    }
+			$sender->sendMessage(TextFormat::GREEN . "CompassBar is now on!");
+		}
 
-    public function onQuit(PlayerQuitEvent $event){
-        $event->getPlayer()->removeBossbar(self::BOSSBAR_ID);
-    }
+		return true;
+	}
+
+	public function removeBossBar(Player $player) : void{
+		$this->sendBossPacket($player, '', BossEventPacket::TYPE_HIDE);
+
+		$pk = new RemoveActorPacket();
+		$pk->entityUniqueId = $this->entityId;
+		$player->sendDataPacket($pk);
+	}
+
+	public function onQuit(PlayerQuitEvent $event){
+		if(!isset($this->enabled[$event->getPlayer()->getLowerCaseName()])){
+			$this->removeBossBar($event->getPlayer());
+		}
+	}
+
+	public function sendBossPacket(Player $player, string $title, int $eventType = BossEventPacket::TYPE_TITLE) : void{
+		$pk = new BossEventPacket();
+		$pk->bossEid = $this->entityId;
+		$pk->eventType = $eventType;
+		$pk->title = $title;
+
+		if($eventType === BossEventPacket::TYPE_SHOW){
+			$pk->healthPercent = 1.0;
+			$pk->unknownShort = $pk->color = $pk->overlay = 0;
+		}
+
+		$player->sendDataPacket($pk);
+	}
 }
--- src/Enes5519/CompassBar/ShowBarTask.php
+++ src/Enes5519/CompassBar/ShowBarTask.php
@@ -4,37 +4,29 @@ declare(strict_types=1);
 
 namespace Enes5519\CompassBar;
 
-use pocketmine\Player;
 use pocketmine\scheduler\Task;
 
 class ShowBarTask extends Task{
+	/** @var CompassBar */
+	private $plugin;
 
-    /** @var CompassBar */
-    private $plugin;
+	public function __construct(CompassBar $plugin){
+		$this->plugin = $plugin;
+	}
 
-    public function __construct(CompassBar $plugin){
-        $this->plugin = $plugin;
-    }
+	public function onRun(int $currentTick){
+		foreach($this->plugin->enabled as $player){
+			if(!$player->isClosed()){
+				$this->plugin->sendBossPacket($player, Utils::getCompass($player->yaw));
+			}else{
+				$this->plugin->removeBossBar($player);
+			}
+		}
+	}
 
-    public function onRun(int $currentTick){
-        $bossBar = $this->plugin->getBossBar();
-        /** @var Player $viewer */
-        foreach($bossBar->getViewers() as $viewer){
-            if(!$viewer->isClosed()){
-                $bossBar->setTitle(Utils::getCompass($viewer->yaw));
-                $bossBar->updateFor($viewer);
-            }else{
-                $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
-            }
-        }
-    }
-
-    public function onCancel(){
-        $bossBar = $this->plugin->getBossBar();
-        /** @var Player $viewer */
-        foreach($bossBar->getViewers() as $viewer){
-            $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
-        }
-    }
-
+	public function onCancel(){
+		foreach($this->plugin->enabled as $player){
+			$this->plugin->removeBossBar($player);
+		}
+	}
 }
